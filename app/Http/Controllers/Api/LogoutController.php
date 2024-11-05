2eab8760aa5c0f503388dd164616bac6931863f7
rheznandyaa31 <kuliahtugas180@gmail.com>
2024-11-06 03:52:14 +0700

    Jobsheet 10 1 Praktikum 3

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -9,7 +9,6 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
-
 class LogoutController extends Controller
 {
     public function __invoke(Request $request)
